c68f26f0dc964bfe88083853ce3955fec1692504
JonathanFL <j.lydemann@gmail.com>
2019-02-05 11:53:14 +0100

    Added files from zip given in class.

--- ReportGenerator/ReportGenerator/Program.cs
+++ ReportGenerator/ReportGenerator/Program.cs
@@ -10,7 +10,24 @@ namespace ReportGenerator
     {
         static void Main(string[] args)
         {
+            var db = new EmployeeDB();
 
+            // Add some employees
+            db.AddEmployee(new Employee("Anne", 3000));
+            db.AddEmployee(new Employee("Berit", 2000));
+            db.AddEmployee(new Employee("Christel", 1000));
+
+            var reportGenerator = new ReportGenerator(db);
+
+            // Create a default (name-first) report
+            reportGenerator.CompileReport();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            // Create a salary-first report
+            reportGenerator.SetOutputFormat(ReportOutputFormatType.SalaryFirst);
+            reportGenerator.CompileReport();
         }
     }
 }
